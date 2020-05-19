1db31fddb846c812374686be55b2aea14f1e9d04
dulaj dhanushanka <dulajdhanushanka@gmail.com>
2020-05-19 14:02:59 +0530

    ui fix

--- lib/views/PutImage.dart
+++ lib/views/PutImage.dart
@@ -24,121 +24,148 @@ class _PutYouBestState extends State<PutYouBest> {
         child: Scaffold(
       backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              // color: Colors.redAccent,
-              height: blockHeight * 10,
-            ),
-            Container(
-              // color: Colors.greenAccent,
-              height: blockHeight * 6,
-              margin: EdgeInsets.symmetric(horizontal: margin),
-              alignment: Alignment.center,
-              child: AutoSizeText(
-                "Put your best forward",
-                style: GoogleFonts.montserrat(
-                    fontSize: 25, fontWeight: FontWeight.w500),
-                minFontSize: 16,
-              ),
-            ),
-            Container(
-              // color: Colors.redAccent,
-              height: blockHeight * 5,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: blockWidth)),
-              height: blockHeight * 20,
-              width: blockHeight * 20,
-              margin: EdgeInsets.symmetric(horizontal: margin),
-              alignment: Alignment.center,
-              child: SizedBox.expand(
-                child: ButtonTheme(
-                  child: RaisedButton(
-                    color: Colors.white,
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.add,
-                      size: blockHeight * 10,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: blockHeight * 5,
-              // color: Colors.greenAccent,
-            ),
-            Container(
-              margin: EdgeInsets.only(left: margin),
-              height: blockHeight * 10,
-              // padding: EdgeInsets.all(blockHeight),
+          child: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: 0.3,
+            child: Container(
+              height: blockHeight * 100,
+              width: blockWidth * 100,
               // color: Colors.red,
-              alignment: Alignment.centerLeft,
-              child: HorizontalImagesView(blockHeight),
-            ),
-            Container(
-              // color: Colors.redAccent,
-              height: blockHeight * 5,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: margin),
-              // color: Colors.greenAccent,
-              height: blockHeight * 10,
               alignment: Alignment.center,
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    // enabledBorder: UnderlineInputBorder(
-                    //   borderSide: BorderSide(color: Colors.red, width: 5.0),
-                    // ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 3),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 3),
-                    ),
-                    hintText: "Name",
-                    hintStyle: GoogleFonts.montserrat(
-                        fontSize: 20, fontWeight: FontWeight.w500),
-                    // border: UnderlineInputBorder(),
-                    contentPadding: EdgeInsets.only(left: 10)),
+              child: Image.asset(
+                "assets/logo2.png",
+                height: blockHeight * 100,
+                width: blockWidth * 100,
+                fit: BoxFit.cover,
               ),
             ),
-            Container(
-              // color: Colors.redAccent,
-              height: blockHeight * 5,
-            ),
-            Container(
-              color: Colors.redAccent,
-              height: blockHeight * 5,
-              width: blockWidth * 40,
-              alignment: Alignment.center,
-              child: SizedBox.expand(
-                child: ButtonTheme(
-                  child: RaisedButton(
-                    color: Colors.grey,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProfileInfo()));
-                    },
-                    child: AutoSizeText(
-                      "NEXT >",
-                      style: GoogleFonts.montserrat(
-                          fontSize: 20, fontWeight: FontWeight.w500),
-                      minFontSize: 15,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                // color: Colors.redAccent,
+                height: blockHeight * 10,
+              ),
+              Container(
+                // color: Colors.greenAccent,
+                height: blockHeight * 6,
+                margin: EdgeInsets.symmetric(horizontal: margin),
+                alignment: Alignment.center,
+                child: AutoSizeText(
+                  "Put your best forward",
+                  style: GoogleFonts.montserrat(
+                      fontSize: 25, fontWeight: FontWeight.w500),
+                  minFontSize: 16,
+                ),
+              ),
+              Container(
+                // color: Colors.redAccent,
+                height: blockHeight * 5,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    // border: Border.all(color: Colors.black, width: blockWidth)
+                    ),
+                height: blockHeight * 20,
+                width: blockHeight * 20,
+                margin: EdgeInsets.symmetric(horizontal: margin),
+                alignment: Alignment.center,
+                child: SizedBox.expand(
+                  child: ButtonTheme(
+                    child: RaisedButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.add,
+                        size: blockHeight * 10,
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
+              Container(
+                height: blockHeight * 5,
+                // color: Colors.greenAccent,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: margin),
+                height: blockHeight * 12,
+                // padding: EdgeInsets.all(blockHeight),
+                // color: Colors.red,
+                alignment: Alignment.centerLeft,
+                child: HorizontalImagesView(blockHeight),
+              ),
+              Container(
+                // color: Colors.redAccent,
+                height: blockHeight * 5,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: margin),
+                // color: Colors.greenAccent,
+                height: blockHeight * 10,
+                alignment: Alignment.center,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.red, width: 5.0),
+                      // ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 3),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 3),
+                      ),
+                      hintText: "Name",
+                      hintStyle: GoogleFonts.montserrat(
+                          fontSize: 20, fontWeight: FontWeight.w500),
+                      // border: UnderlineInputBorder(),
+                      contentPadding: EdgeInsets.only(left: 10)),
+                ),
+              ),
+              Container(
+                // color: Colors.redAccent,
+                height: blockHeight * 5,
+              ),
+              Container(
+                // color: Colors.redAccent,
+                height: blockHeight * 5,
+                width: blockWidth * 40,
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ProfileInfo()));
+                  },
+                  child: Container(
+                      // color: Colors.blue,
+
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(colors: [
+                            Color(0xffec375c),
+                            Color(0xfff46163),
+                            Color(0xfff98467)
+                          ])),
+                      height: 5 * blockHeight,
+                      width: blockWidth * 40,
+                      margin: EdgeInsets.symmetric(horizontal: margin),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "NEXT",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
     ));
   }
 
@@ -147,61 +174,60 @@ class _PutYouBestState extends State<PutYouBest> {
       // This next line does the trick.
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
+        Card(
+          child: Container(
+            // color: Colors.yellow,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            height: blockHeight * 10,
+            width: blockHeight * 10,
 
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+          ),
         ),
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
+        Card(
+          child: Container(
+            // color: Colors.yellow,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            height: blockHeight * 10,
+            width: blockHeight * 10,
 
-          decoration: BoxDecoration(
-              // color: Colors.yellow,
-
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+          ),
         ),
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
+        Card(
+          child: Container(
+            // color: Colors.yellow,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            height: blockHeight * 10,
+            width: blockHeight * 10,
 
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+          ),
         ),
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
+        Card(
+          child: Container(
+            // color: Colors.yellow,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            height: blockHeight * 10,
+            width: blockHeight * 10,
 
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+          ),
         ),
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
+        Card(
+          child: Container(
+            // color: Colors.yellow,
+            margin: EdgeInsets.symmetric(horizontal: 5),
+            height: blockHeight * 10,
+            width: blockHeight * 10,
 
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
-        ),
-        Container(
-          // color: Colors.yellow,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          height: blockHeight * 10,
-          width: blockHeight * 10,
-
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey, width: blockHeight / 2)),
+          ),
         ),
       ],
     );
